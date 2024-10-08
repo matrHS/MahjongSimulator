e4c860458921796f2545ec496ac9720461af982a
matrHS <matthew.r.hunt@gmail.com>
2024-10-08 10:58:05 +0200

    refactored to use full hand instead of checking per suit

--- src/main/java/tv/laptopgaming/Logic/WinChecker.java
+++ src/main/java/tv/laptopgaming/Logic/WinChecker.java
@@ -6,11 +6,10 @@ import java.util.List;
 import java.util.TreeSet;
 import tv.laptopgaming.Entity.Hand;
 import tv.laptopgaming.Entity.NumberTile;
-import tv.laptopgaming.Entity.Suit;
 import tv.laptopgaming.Entity.Tile;
 
 public class WinChecker {
-  
+
   static TileHelper tileHelper = new TileHelper();
 
   /**
@@ -23,13 +22,13 @@ public class WinChecker {
     int pairs = 0;
     Hand workingHand = hand;
 
-    for (int i = 0; i < hand.getTiles().size()-1; i++) {
-      if (hand.getTiles().get(i).toString().equals(hand.getTiles().get(i+1).toString())) {
+    for (int i = 0; i < hand.getTiles().size() - 1; i++) {
+      if (hand.getTiles().get(i).toString().equals(hand.getTiles().get(i + 1).toString())) {
         pairs++;
         i++;
       }
     }
-    
+
     return pairs;
   }
 
@@ -43,8 +42,8 @@ public class WinChecker {
     int triple = 0;
 
     for (int i = 0; i < hand.getTiles().size() - 2; i++) {
-      if (hand.getTiles().get(i).toString().equals(hand.getTiles().get(i+1).toString()) 
-          && hand.getTiles().get(i).toString().equals(hand.getTiles().get(i+2).toString())) {
+      if (hand.getTiles().get(i).toString().equals(hand.getTiles().get(i + 1).toString())
+          && hand.getTiles().get(i).toString().equals(hand.getTiles().get(i + 2).toString())) {
         triple++;
         i += 2;
       }
@@ -71,60 +70,62 @@ public class WinChecker {
   public int seriesCounter(Hand hand) {
     int series = 0;
     List<TreeSet<Tile>> tileSets = new ArrayList<>();
-    // Instantiate the working hand
-    List<Tile> workingHand = hand.getTilesOfSuit(Suit.Bamboo);
-    for (Suit suit : Suit.values()) {
-      workingHand = hand.getTilesOfSuit(suit);
-      
-      // Exits if there are no number tiles in the hand (Honor tiles cant be series)
-      if (tileHelper.tileListOnlyNumberTiles(workingHand)) {
-        for (Tile tile : workingHand) {
-          // Adds the first tile to the first set (skips tile after adding it to the first set)
-          if (tileSets.isEmpty()) {
-            tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
-            tileSets.getFirst().add(tile);
+    List<Tile> workingHand = hand.getTiles();
+    for (Tile tile : workingHand) {
+      // Exits the checks if the tile is an honor tile as they cannot be in series
+      if (!(tile instanceof NumberTile)) {
+        break;
+      }
+
+      // Adds the first tile to the first set (skips tile after adding it to the first set)
+      if (tileSets.isEmpty()) {
+        tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
+        tileSets.getFirst().add(tile);
+        continue;
+      }
+      // Loops equals to the number of sets
+      for (int i = 0; i < tileSets.size(); i++) {
+
+        // Added a check to see if there is another set in list.
+        // If there is, it will continue to the next set. 
+        // if not, it will check for duplicate and add to a new set
+        if (tileSets.get(i).size() == 3) {
+          if (tileSets.size() > i + 1) {
             continue;
           }
-          // Loops equals to the number of sets
-          for (int i = 0; i < tileSets.size(); i++) {
-            
-            // Added a check to see if there is another set in list.
-            // If there is, it will continue to the next set. 
-            // if not, it will check for duplicate and add to a new set
-            if (tileSets.get(i).size() == 3) {
-              if (tileSets.size() > i + 1) {
-                continue;
-              }
-            }
-            // Check if the current tile is in series with the last tile in current set
-            if (((NumberTile) tileSets.get(i).getLast()).getNumber() + 1 == ((NumberTile) tile).getNumber()) {
-              tileSets.get(i).add(tile);
-              if (tileSets.get(i).size() == 3) {
-                series++;
-              }
-              break;
-            }
-            // Check if current tile is a duplicate with last tile in current set
-            if (((NumberTile) tileSets.get(i).getLast()).getNumber() == ((NumberTile) tile).getNumber()) {
-              // Check if there is another set to check against
-              if (tileSets.size() > i + 1) {
-                continue;
-              }
-              // If there is no other set, add the tile to a new set
-              tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
-              tileSets.getLast().add(tile);
-              break;
-            }
-            // If the tile was not a duplicate, add the tile as a new set.
-            tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
-            tileSets.getLast().add(tile);
-            break;
-          }
         }
-        // Clears the tileSets list for the next suit
-        tileSets.clear();
+        // Check if the current tile is in series with the last tile in current set
+        // Also checks if the suit is the same
+        if (((NumberTile) tileSets.get(i).getLast()).getNumber() + 1 ==
+            ((NumberTile) tile).getNumber()
+            && tileSets.get(i).getLast().getSuit() == tile.getSuit()) {
+          tileSets.get(i).add(tile);
+          if (tileSets.get(i).size() == 3) {
+            series++;
+          }
+          break;
+        }
+        // Check if current tile is a duplicate with last tile in current set
+        if (((NumberTile) tileSets.get(i).getLast()).getNumber() ==
+            ((NumberTile) tile).getNumber()) {
+          // Check if there is another set to check against
+          if (tileSets.size() > i + 1) {
+            continue;
+          }
+          // If there is no other set, add the tile to a new set
+          tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
+          tileSets.getLast().add(tile);
+          break;
+        }
+        // If the tile was not a duplicate, add the tile as a new set.
+        tileSets.add(new TreeSet<>(Comparator.comparing(Tile::toString)));
+        tileSets.getLast().add(tile);
+        break;
+
       }
+      // Clears the tileSets list for the next suit
     }
+    System.out.println(tileSets);
     return series;
   }
 
@@ -138,13 +139,13 @@ public class WinChecker {
   public static boolean isWinningHand(Hand hand) {
     boolean winningHand = false;
     int pairs = pairCounter(hand);
-    
-    
+
+
     if (pairs == 7) {
       winningHand = true;
     }
-    
+
     return winningHand;
   }
-  
+
 }
